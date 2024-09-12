58b3ee8c20e12c4e2609098f23e16df613031bbf
Robert Takacs <RoTak@protonmail.com>
2024-09-12 20:53:00 +0300

    Fix broken images after subdomain changes

--- posts/recommended/games-of-science.php
+++ posts/recommended/games-of-science.php
@@ -1,104 +1,132 @@
-<div class = "image-banner">
-    <img src = "https://www.rotak.ro/backend/public/images/posts/games-of-science-logo.webp" alt = "Logo-ul Games of Science"/>
+<div class="image-banner">
+    <img src="https://backend.rotak.ro/images/posts/games-of-science-logo.webp" alt="Logo-ul Games of Science" />
 </div>
 <section>
     <h3>Ce înseamnă pentru mine Games of Science?</h3>
-    <p>Games of Science a fost pentru mine, în anul 2023, o primă introducere în Public Speaking și în metodele de a-ți face discursul, în orice ar consta acesta, nu numai auzit, dar și ascultat de către public. Evenimentul începe cu o zi întreagă de training interactiv și social, cu o eficiență incredibilă, care prezintă, pe scurt, tot ce e necesar pentru un discurs de succes. </p>
-    <blockquote cite = "https://www.gamesofscience.org/despre-gos">Games of Science este un proiect de promovare a comunicării științei pe înțelesul publicului larg, constituit din sesiuni de training cu tineri oameni de ştiinţă, la capătul cărora participanții se întrec într-un concurs de prezentări științifice.</blockquote>
-    <p>Prima mea participare, și unica până în momentul de față, a avut loc în momentul în care mă aflam în anul 1 de studii de licență. La a doua ediție a concursului, am luat parte la training-uri și am vorbit despre un subiect care era foarte în temă în momentul respectiv. Inteligența artificială - planul meu a fost să demaschez miturile existente și să explic modul în care funcționează algoritmii AI. Nu m-am calificat la finală, dar am obținut mult mai multe: discursurile celor cu care am participat la ediția locală a evenimentului m-au învățat despre fizică, psihologie, informatică, și nu numai. De atunci, consider acest concurs o adevărată comoară de cunoștințe.</p>
+    <p>Games of Science a fost pentru mine, în anul 2023, o primă introducere în Public Speaking și în metodele de a-ți
+        face discursul, în orice ar consta acesta, nu numai auzit, dar și ascultat de către public. Evenimentul începe
+        cu o zi întreagă de training interactiv și social, cu o eficiență incredibilă, care prezintă, pe scurt, tot ce e
+        necesar pentru un discurs de succes. </p>
+    <blockquote cite="https://www.gamesofscience.org/despre-gos">Games of Science este un proiect de promovare a
+        comunicării științei pe înțelesul publicului larg, constituit din sesiuni de training cu tineri oameni de
+        ştiinţă, la capătul cărora participanții se întrec într-un concurs de prezentări științifice.</blockquote>
+    <p>Prima mea participare, și unica până în momentul de față, a avut loc în momentul în care mă aflam în anul 1 de
+        studii de licență. La a doua ediție a concursului, am luat parte la training-uri și am vorbit despre un subiect
+        care era foarte în temă în momentul respectiv. Inteligența artificială - planul meu a fost să demaschez miturile
+        existente și să explic modul în care funcționează algoritmii AI. Nu m-am calificat la finală, dar am obținut
+        mult mai multe: discursurile celor cu care am participat la ediția locală a evenimentului m-au învățat despre
+        fizică, psihologie, informatică, și nu numai. De atunci, consider acest concurs o adevărată comoară de
+        cunoștințe.</p>
 </section>
-<div class = "image-responsive my-2">
+<div class="image-responsive my-2">
     <figure>
-        <img src = "https://www.rotak.ro/backend/public/images/posts/games-of-science-2023.webp" alt = "Florin Teleanu, câștigător al ediției 2023 a Games of Science"/>
-        <figcaption>Florin Teleanu, câștigătorul ediției 2023. Preluat de pe gamesofscience.org</figcaption>    
+        <img src="https://backend.rotak.ro/images/posts/games-of-science-2023.webp"
+            alt="Florin Teleanu, câștigător al ediției 2023 a Games of Science" />
+        <figcaption>Florin Teleanu, câștigătorul ediției 2023. Preluat de pe gamesofscience.org</figcaption>
     </figure>
 </div>
 <section>
     <h3>De ce GoS special?</h3>
-    <p>Formatul concursului e ce consider eu că îl diferențiază de altele. E extrem de brutal, dar totuși incredibil de simplu: Cum așa?</p>
-    <p>În viață, e necesar să captezi atenția celor cu care încerci să comunici. Nimeni nu are timp, toată lumea este stresată. Games of Science demonstrează că 15 secunde sunt de ajuns. Există 16 participanți în concurs, care se întrec 2 câte 2, în stil eliminatoriu. În etapa 1, participanții au doar 15 secunde să își prezinte subiectul și să convingă juriul că ei sunt cei care merită ascultați mai mult. Cei care pierd, sunt eliminați. În 15 secunde, poți pierde totul.</p>
-    <p>Etapa a doua permite concurenților să își prezinte subiectul în 30 de secunde, sferturile de finală în 60 de secunde. Doar în semifinală și în finală participanții au timp să detalieze subiectul lor. În finală, participanții au 3 minute ca să detalieze subiectul lor de cercetare.</p>
-    <p>O paranteză pe care o consider importantă, sunt bucuros să știu că am fost eliminat din concurs de persoana care mai apoi a câștigat ediția 2023 a concursului, Florin Teleanu, cercetător în cadrul proiectului ELI-NP, cu discursul său despre legătura dintre evenimentele cuantice și fenomenele biologice.</p>
+    <p>Formatul concursului e ce consider eu că îl diferențiază de altele. E extrem de brutal, dar totuși incredibil de
+        simplu: Cum așa?</p>
+    <p>În viață, e necesar să captezi atenția celor cu care încerci să comunici. Nimeni nu are timp, toată lumea este
+        stresată. Games of Science demonstrează că 15 secunde sunt de ajuns. Există 16 participanți în concurs, care se
+        întrec 2 câte 2, în stil eliminatoriu. În etapa 1, participanții au doar 15 secunde să își prezinte subiectul și
+        să convingă juriul că ei sunt cei care merită ascultați mai mult. Cei care pierd, sunt eliminați. În 15 secunde,
+        poți pierde totul.</p>
+    <p>Etapa a doua permite concurenților să își prezinte subiectul în 30 de secunde, sferturile de finală în 60 de
+        secunde. Doar în semifinală și în finală participanții au timp să detalieze subiectul lor. În finală,
+        participanții au 3 minute ca să detalieze subiectul lor de cercetare.</p>
+    <p>O paranteză pe care o consider importantă, sunt bucuros să știu că am fost eliminat din concurs de persoana care
+        mai apoi a câștigat ediția 2023 a concursului, Florin Teleanu, cercetător în cadrul proiectului ELI-NP, cu
+        discursul său despre legătura dintre evenimentele cuantice și fenomenele biologice.</p>
 </section>
-<div class = "image-responsive my-2">
+<div class="image-responsive my-2">
     <figure>
-        <img src = "https://www.rotak.ro/backend/public/images/posts/games-of-science-2024.webp" alt = "Otilia Pintilii, câștigătoarea ediției 2024 a Games of Science"/>
-        <figcaption>Otilia Pintilii, câștigătoarea ediției 2024. Fotografie proprie.</figcaption> 
+        <img src="https://backend.rotak.ro/images/posts/games-of-science-2024.webp"
+            alt="Otilia Pintilii, câștigătoarea ediției 2024 a Games of Science" />
+        <figcaption>Otilia Pintilii, câștigătoarea ediției 2024. Fotografie proprie.</figcaption>
     </figure>
 </div>
 <section>
     <h3>Etapa 2024 a Games of Science</h3>
-    <p>Anul acesta, mă bucur să fi fost numit ambasador al Games of Science. Am reușit să promovez evenimentul în cercul meu, iar asta a adus-o în concurs pe Otilia Pintilii. Aceasta a câștigat etapa din Iași a evenimentului, iar mai apoi a cucerit juriul finalei și a câștigat ediția a 3-a a Games of Science, cu un discurs despre dezvoltarea unei inimi artificiale de titan.</p>
-    <p>Pe lângă participanți, concursul a avut o organizare perfectă și extrem de profesională, și sunt sigur că acest lucru se va menține și în următorii ani.</p>
+    <p>Anul acesta, mă bucur să fi fost numit ambasador al Games of Science. Am reușit să promovez evenimentul în cercul
+        meu, iar asta a adus-o în concurs pe Otilia Pintilii. Aceasta a câștigat etapa din Iași a evenimentului, iar mai
+        apoi a cucerit juriul finalei și a câștigat ediția a 3-a a Games of Science, cu un discurs despre dezvoltarea
+        unei inimi artificiale de titan.</p>
+    <p>Pe lângă participanți, concursul a avut o organizare perfectă și extrem de profesională, și sunt sigur că acest
+        lucru se va menține și în următorii ani.</p>
 </section>
-<div class = "image-responsive my-2">
+<div class="image-responsive my-2">
     <figure>
-        <img src = "https://www.rotak.ro/backend/public/images/posts/games-of-science-me.webp" alt = "Eu, la etapa locală a evenimentului din 2023"/>
-        <figcaption>Eu, la etapa locală a evenimentului din 2023.</figcaption>    
+        <img src="https://backend.rotak.ro/images/posts/games-of-science-me.webp"
+            alt="Eu, la etapa locală a evenimentului din 2023" />
+        <figcaption>Eu, la etapa locală a evenimentului din 2023.</figcaption>
     </figure>
 </div>
 <section>
     <h3>Mulțumiri</h3>
     <p>
-        Acest concurs nu ar fi posibil fără <a href = "https://www.gamesofscience.org/echipa" target = "_blank">echipa de facilitatori și traineri ai concursului</a>: Ada Roseti, Carmen Nicolae, Andrada Balmez, Laura Hroștea, Cezara Bucătaru, Andra Păsărin, Mălina Crudu, Brenda Piuaru, Raluca Bârsan, dar și organizatorii Iulia Scărlătescu, Diana Maxim, Daniel Milenovici, Gabriel Oteșanu, Tudor Osiescu. <i>(Aceste nume au fost preluate de pe site-ul gamesofscience.org la data de 17.05.2025)</i>
+        Acest concurs nu ar fi posibil fără <a href="https://www.gamesofscience.org/echipa" target="_blank">echipa de
+            facilitatori și traineri ai concursului</a>: Ada Roseti, Carmen Nicolae, Andrada Balmez, Laura Hroștea,
+        Cezara Bucătaru, Andra Păsărin, Mălina Crudu, Brenda Piuaru, Raluca Bârsan, dar și organizatorii Iulia
+        Scărlătescu, Diana Maxim, Daniel Milenovici, Gabriel Oteșanu, Tudor Osiescu. <i>(Aceste nume au fost preluate de
+            pe site-ul gamesofscience.org la data de 17.05.2025)</i>
     </p>
     <p>
-        În acest sens, vreau să le mulțumesc tuturor celor care organizează, participă sau susțin acest concurs. Mulțumesc în special Adei pentru toată susținerea și sfaturile pe care mi le-a oferit, atât în contextul Games of Science, cât și în afară, și mulțumiri în special lui Carmen și lui Gabriel. Îi consider niște persoane exemplu, și niște oameni adevărați. 
+        În acest sens, vreau să le mulțumesc tuturor celor care organizează, participă sau susțin acest concurs.
+        Mulțumesc în special Adei pentru toată susținerea și sfaturile pe care mi le-a oferit, atât în contextul Games
+        of Science, cât și în afară, și mulțumiri în special lui Carmen și lui Gabriel. Îi consider niște persoane
+        exemplu, și niște oameni adevărați.
     </p>
-    <p>Mult mai multe despre eveniment pot fi găsite pe site-ul oficial <a href = "https://www.gamesofscience.org/" target = "_blank">Games of Science</a>.</p>
+    <p>Mult mai multe despre eveniment pot fi găsite pe site-ul oficial <a href="https://www.gamesofscience.org/"
+            target="_blank">Games of Science</a>.</p>
 </section>
 
 
 <style>
-
-    
-    .image-responsive, .image-banner
-    {
+    .image-responsive,
+    .image-banner {
         max-width: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
     }
 
-    .image-responsive figure figcaption, .image-banner figure figcaption
-    {
+    .image-responsive figure figcaption,
+    .image-banner figure figcaption {
         font-size: 0.8em;
         margin-top: 0.25em;
     }
 
 
-    .image-responsive img
-    {
+    .image-responsive img {
         max-width: 100%;
         height: auto;
         margin: 0 auto;
         display: block;
     }
 
-    @media screen and (min-width: 768px) 
-    {
-        .image-responsive img
-        {
+    @media screen and (min-width: 768px) {
+        .image-responsive img {
             max-width: 500px;
         }
     }
 
-    .image-banner img 
-    {
+    .image-banner img {
         max-width: 100%;
         height: auto;
         margin: 0 auto;
         display: block;
     }
-    
-    blockquote {
-    font-family: 'Georgia', serif;
-    font-style: italic;
-    color: #555;
-    padding: 10px 20px;
-    margin: 20px 0;
-    border-left: 5px solid #ccc;
-    background: #f9f9f9;
-}
 
+    blockquote {
+        font-family: 'Georgia', serif;
+        font-style: italic;
+        color: #555;
+        padding: 10px 20px;
+        margin: 20px 0;
+        border-left: 5px solid #ccc;
+        background: #f9f9f9;
+    }
 </style>
